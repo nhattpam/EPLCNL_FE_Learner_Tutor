9f3e026486d6369c6c3e806218247a6a1d1d22e6
nhattpam <nhat2up@gmail.com>
2024-02-06 08:44:26 +0700

    chat v2

--- fe_learner_tutor/src/components/tutor/forum/EditForum.jsx
+++ fe_learner_tutor/src/components/tutor/forum/EditForum.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Footer from '../Footer';
 import Header from '../Header';
 import Sidebar from '../Sidebar';
-import { Link } from 'react-router-dom';
-import tutorService from '../../../services/tutor.service';
+import accountForumService from '../../../services/account-forum.service';
 import forumService from '../../../services/forum.service';
 
 const EditForum = () => {
     const { forumId } = useParams();
     const [accountForumList, setAccountForumList] = useState([]);
     const [forum, setForum] = useState([]);
+    const tutorId = localStorage.getItem('tutorId');
+    const [errors, setErrors] = useState({});
+    const [msg, setMsg] = useState('');
+    const navigate = useNavigate();
 
     useEffect(() => {
         forumService
@@ -25,62 +28,104 @@ const EditForum = () => {
 
     useEffect(() => {
         if (forumId) {
-          forumService
-            .getForumById(forumId)
+            forumService
+                .getForumById(forumId)
+                .then((res) => {
+                    setForum(res.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, [forumId]);
+
+    //tao account forum
+    const [accountForum, setAccountForum] = useState({
+        tutorId: tutorId,
+        forumId: forumId, // set a default value for minutes
+        message: "",
+    });
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setAccountForum({ ...accountForum, [e.target.name]: value });
+    }
+
+    const submitAccountForum = async (e) => {
+        e.preventDefault();
+    
+        // Save account forum
+        console.log(JSON.stringify(accountForum))
+        const accountForumResponse = await accountForumService.saveAccountForum(accountForum);
+        console.log(accountForumResponse.data);
+    
+        setMsg('AccountForum Added Successfully');
+    
+        // Fetch updated list of messages after sending a message
+        forumService
+            .getAllClassForumsByForum(forumId)
             .then((res) => {
-              setForum(res.data);
+                setAccountForumList(res.data);
             })
             .catch((error) => {
-              console.log(error);
+                console.log(error);
             });
-        }
-      }, [forumId]);
-    return (
-        <>
-            <div id="wrapper">
-                <Header />
-                <Sidebar />
-                <div className="content-page">
-                    <div className="content">
-                        <div className="container-fluid">
-                            <div className="row">
-                                <div className="col-12">
-                                    <div className="page-title-box">
-                                        <div className="page-title-right">
-                                            <ol className="breadcrumb m-0">
-                                            </ol>
-                                        </div>
-                                        <h4 className="page-title">Forum for course {forum.course?.name}</h4>
+    
+        // Clear the message input
+        setAccountForum({ ...accountForum, message: '' });
+    }
+
+
+return (
+    <>
+        <div id="wrapper">
+            <Header />
+            <Sidebar />
+            <div className="content-page">
+                <div className="content">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="page-title-box">
+                                    <div className="page-title-right">
+                                        <ol className="breadcrumb m-0">
+                                        </ol>
                                     </div>
+                                    <h4 className="page-title">Forum for course {forum.course?.name}</h4>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-12">
-                                    <div className="card-box">
-                                        <div className="chat-container">
-                                            {accountForumList.map((accountForum) => (
-                                                <div
-                                                    className={`chat-message ${accountForum.tutor ? 'left' : 'right'}`}
-                                                    key={accountForum.id}
-                                                >
-                                                    <div className="message-sender">
-                                                        {accountForum.tutor?.account?.fullName ?? accountForum.learner?.account?.fullName}
-                                                    </div>
-                                                    <div className="message-content">{accountForum.message}</div>
-                                                    <div className="message-date">{accountForum.messagedDate}</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="card-box">
+                                    <div className="chat-container">
+                                        {accountForumList.map((accountForum) => (
+                                            <div
+                                                className={`chat-message ${accountForum.tutor ? 'left' : 'right'}`}
+                                                key={accountForum.id}
+                                            >
+                                                <div className="message-sender">
+                                                    {accountForum.tutor?.account?.fullName ?? accountForum.learner?.account?.fullName}
                                                 </div>
-                                            ))}
-                                        </div>
+                                                <div className="message-content">{accountForum.message}</div>
+                                                <div className="message-date">{accountForum.messagedDate}</div>
+                                            </div>
+                                        ))}
+                                        <form class="msger-inputarea" onSubmit={submitAccountForum} >
+                                            <input type="text" class="msger-input" placeholder="Enter your message..." name='message' id='message' value={accountForum.message} onChange={(e) => handleChange(e)} />
+                                            <button type="submit" class="msger-send-btn">Send</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <Footer />
             </div>
-            <style>
-                {`
+            <Footer />
+        </div>
+        <style>
+            {`
                     /* Add these styles to your existing CSS file or create a new one */
 
                     .chat-container {
@@ -121,13 +166,41 @@ const EditForum = () => {
                         bottom: 5px;
                         right: 5px;
                       }
+
+                      .msger-inputarea {
+                        display: flex;
+                        padding: 10px;
+                        border-top: var(--border);
+                        background: #eee;
+                      }
+                      .msger-inputarea * {
+                        padding: 10px;
+                        border: none;
+                        border-radius: 3px;
+                        font-size: 1em;
+                      }
+                      .msger-input {
+                        flex: 1;
+                        background: #ddd;
+                      }
+                      .msger-send-btn {
+                        margin-left: 10px;
+                        background: rgb(0, 196, 65);
+                        color: #fff;
+                        font-weight: bold;
+                        cursor: pointer;
+                        transition: background 0.23s;
+                      }
+                      .msger-send-btn:hover {
+                        background: rgb(0, 180, 50);
+                      }
                     
                     /* Adjust the styling according to your design preferences */
                     
                 `}
-            </style>
-        </>
-    );
+        </style>
+    </>
+);
 };
 
 export default EditForum;
---
+++ fe_learner_tutor/src/services/account-forum.service.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+// const API_URL = "https://localhost:7215/api";
+const API_URL = "https://nhatpmse.twentytwo.asia/api";
+
+
+class AccountForumService {
+
+  token = '';
+
+  setToken(token) {
+    this.token = token;
+  }
+
+  saveAccountForum(accForum) {
+    return axios.post(API_URL + "/account-forums/", accForum, {
+      headers: {
+        Authorization: `Bearer ${this.token}` // Include the bearer token in the headers
+      }
+    });
+  }
+
+  
+}
+export default new AccountForumService;
