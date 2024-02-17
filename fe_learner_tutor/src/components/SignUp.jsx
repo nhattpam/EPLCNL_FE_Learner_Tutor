d53356ac2380d14f09722936418ec654b3c3d65d
Pham Thong <cothongpham@gmail.com>
2024-02-17 15:36:40 +0700

    fix inactive tutor

--- fe_learner_tutor/src/components/SignUp.jsx
+++ fe_learner_tutor/src/components/SignUp.jsx
@@ -71,6 +71,7 @@ const SignUp = () => {
         roleId = "f3db0ef2-7f03-4728-a868-aacbe76891a8";
       } else if (document.getElementById("tutor").checked) {
         roleId = "1dc7ed61-a13d-4cfc-9e3e-2159f61bad3b";
+        account.isActive = false;
       }
   
       try {
