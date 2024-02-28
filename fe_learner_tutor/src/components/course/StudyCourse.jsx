8fb16cc3c27f08ffc8e42ebe763769ca7605caae
Nhat Pham <nhat2up@gmail.com>
2024-02-28 16:55:06 +0700

    study course v1.1

--- fe_learner_tutor/src/components/course/StudyCourse.jsx
+++ fe_learner_tutor/src/components/course/StudyCourse.jsx
@@ -88,6 +88,12 @@ const StudyCourse = () => {
         }
     }, [selectedModule]);
 
+    // Function to handle click on a module card to toggle expansion
+    const handleModuleCardClick = (moduleId) => {
+        toggleModuleExpansion(moduleId);
+        setSelectedModule(moduleList.find(module => module.id === moduleId));
+    };
+
     return (
         <>
             <Header />
@@ -97,55 +103,59 @@ const StudyCourse = () => {
                         <h4 style={{ color: '#fff' }}>{course.name}</h4>
                     </div>
                 </div>
-                <section id="courses" className="courses">
+                <section id="courses" className="courses" style={{ marginTop: '-60px' }}>
                     <div className='row'>
                         <div className="col-md-8">
                             {/* Course Content */}
                         </div>
 
-                        <div className="col-md-4" style={{ textAlign: 'left' , marginTop: '-60px'}}> {/* Adjusted width for sidebar */}
+                        <div className="col-md-4" style={{ textAlign: 'left' }}> {/* Adjusted width for sidebar */}
                             {/* Right Sidebar Content Here */}
                             <div style={{ background: '#f8f9fa', padding: '20px', border: '1px solid #ddd', textAlign: 'left' }}>
                                 {/* Add your sidebar content here */}
                                 <h4 style={{ fontWeight: 'bold' }}>Course content</h4>
                                 {moduleList && moduleList.length > 0 && moduleList.map((module, index) => (
-                                    <div key={module.id}>
-                                        <h4
-                                            className="module-title" // Add CSS class for styling
-                                            onClick={() => setSelectedModule(module)}
+                                    <div key={module.id} className="card-container" style={{ marginBottom: '5px' }}>
+                                        <div
+                                            className={`card module-title ${expandedModules.includes(module.id) ? 'expanded' : ''}`}
+                                            onClick={() => handleModuleCardClick(module.id)} style={{ marginBottom: '5px' }}
                                         >
-                                            <span>Section {index + 1}: {module.name}</span>
-                                            <span onClick={() => toggleModuleExpansion(module.id)}>
-                                                {expandedModules.includes(module.id) ? '-' : '+'}
-                                            </span>
-                                        </h4>
+                                            <div className="card-body" style={{ padding: '10px' }}>
+                                                <h4 className="card-title" >Section {index + 1}: {module.name}</h4>
+                                                <span>{expandedModules.includes(module.id) ? '-' : '+'}</span>
+                                            </div>
+                                        </div>
                                         {selectedModule && selectedModule.id === module.id && expandedModules.includes(module.id) && (
-                                            <>
-                                                {moduleContent.lessons && moduleContent.lessons.length > 0 && (
-                                                    <ul className="module-list"> {/* Add CSS class for styling */}
-                                                        {moduleContent.lessons.map((lesson, index) => (
-                                                            <li key={index}>{lesson.name}</li>
-                                                        ))}
-                                                    </ul>
-                                                )}
-
-                                                {moduleContent.assignments && moduleContent.assignments.length > 0 && (
-                                                    <ul className="module-list"> {/* Add CSS class for styling */}
-                                                        {moduleContent.assignments.map((assignment, index) => (
-                                                            <li key={index}>{assignment.questionText}</li>
-                                                        ))}
-                                                    </ul>
-                                                )}
-
-                                                {moduleContent.quizzes && moduleContent.quizzes.length > 0 && (
-                                                    <ul className="module-list"> {/* Add CSS class for styling */}
-                                                        {moduleContent.quizzes.map((quiz, index) => (
-                                                            <li key={index}>{quiz.name}</li>
-                                                        ))}
-                                                    </ul>
-                                                )}
-                                            </>
+                                            <div className="card-content">
+                                                {/* Combine all items into a single array */}
+                                                {moduleContent.lessons.map((lesson, index) => (
+                                                    <div key={`lesson_${index}`} className="card" style={{ marginBottom: '5px' }}>
+                                                        <div className="card-body">{index + 1}. {lesson.name}</div>
+                                                        <div className="card-body" style={{ marginTop: '-40px' }}>
+                                                            <i className="fas fa-file-video"></i>
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                                {moduleContent.assignments.map((assignment, index) => (
+                                                    <div key={`assignment_${index}`} className="card" style={{ marginBottom: '5px' }}>
+                                                        <div className="card-body">{moduleContent.lessons.length + index + 1}. {assignment.questionText}</div>
+                                                        <div className="card-body" style={{ marginTop: '-40px' }}>
+                                                            <i className="fab fa-wpforms"></i> {assignment.deadline} mins
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                                {moduleContent.quizzes.map((quiz, index) => (
+                                                    <div key={`quiz_${index}`} className="card" style={{ marginBottom: '5px' }}>
+                                                        <div className="card-body">{moduleContent.lessons.length + moduleContent.assignments.length + index + 1}. {quiz.name}</div>
+                                                        <div className="card-body" style={{ marginTop: '-40px' }}>
+                                                            <i className="far fa-question-circle"></i> {quiz.deadline} mins
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                            </div>
                                         )}
+
+
                                     </div>
                                 ))}
                             </div>
@@ -168,7 +178,17 @@ const StudyCourse = () => {
                     cursor: pointer;
                 }
                 
-                
+                .card.module-title {
+    background-color: #FFF0D6; /* Darker background color */
+    color: #000; /* White text color */
+    transition: background-color 0.3s ease; /* Smooth transition effect */
+}
+
+.card.module-title:hover {
+    background-color: #E7E3DC; /* Darker background color on hover */
+    color: #fff
+}
+
             `}
             </style>
         </>
