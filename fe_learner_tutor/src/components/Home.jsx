84726bfe6ea863c0d7a2b7fd18a57c067523c3cf
Pham Thong <cothongpham@gmail.com>
2024-03-01 10:52:35 +0700

    tutors in home

--- fe_learner_tutor/src/components/Home.jsx
+++ fe_learner_tutor/src/components/Home.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import { Link } from 'react-router-dom'
 import courseService from '../services/course.service';
 import accountService from '../services/account.service';
+import tutorService from '../services/tutor.service';
 
 const Home = () => {
 
@@ -45,6 +46,20 @@ const Home = () => {
     }
   }, [courseList]);
 
+  useEffect(() => {
+    const fetchTutors = async () => {
+      try {
+        const res = await tutorService.getAllTutor(); // Add await here
+        const activeTutors = res.data.filter((tutor) => tutor.account.isActive === true);
+        setTutorList(activeTutors);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchTutors();
+  }, []);
+
   return (
     <>
       <Header />
@@ -81,7 +96,7 @@ const Home = () => {
         <section id="counts" className="counts section-bg">
           <div className="container">
             <div className="row counters">
-              
+
             </div>
           </div>
         </section>{/* End Counts Section */}
@@ -257,60 +272,26 @@ const Home = () => {
         <section id="trainers" className="trainers">
           <div className="container" data-aos="fade-up">
             <div className="row" data-aos="zoom-in" data-aos-delay={100}>
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
-                <div className="member">
-                  <img src="assets/img/trainers/trainer-1.jpg" className="img-fluid" alt />
-                  <div className="member-content">
-                    <h4>Walter White</h4>
-                    <span>Web Development</span>
-                    <p>
-                      Magni qui quod omnis unde et eos fuga et exercitationem. Odio veritatis perspiciatis quaerat qui aut aut aut
-                    </p>
-                    <div className="social">
-                      <a href><i className="bi bi-twitter" /></a>
-                      <a href><i className="bi bi-facebook" /></a>
-                      <a href><i className="bi bi-instagram" /></a>
-                      <a href><i className="bi bi-linkedin" /></a>
+              {tutorList.map((tutor, index) => (
+                <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+                  <div className="member">
+                    <img src={tutor.account.imageUrl} className="img-fluid" alt />
+                    <div className="member-content">
+                      <h4>{tutor.account.fullName}</h4>
+                      <span>{tutor.account.email}</span>
+                      
+                      <div className="social">
+                        <a href><i className="bi bi-twitter" /></a>
+                        <a href><i className="bi bi-facebook" /></a>
+                        <a href><i className="bi bi-instagram" /></a>
+                        <a href><i className="bi bi-linkedin" /></a>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
-                <div className="member">
-                  <img src="assets/img/trainers/trainer-2.jpg" className="img-fluid" alt />
-                  <div className="member-content">
-                    <h4>Sarah Jhinson</h4>
-                    <span>Marketing</span>
-                    <p>
-                      Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus
-                    </p>
-                    <div className="social">
-                      <a href><i className="bi bi-twitter" /></a>
-                      <a href><i className="bi bi-facebook" /></a>
-                      <a href><i className="bi bi-instagram" /></a>
-                      <a href><i className="bi bi-linkedin" /></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
-                <div className="member">
-                  <img src="assets/img/trainers/trainer-3.jpg" className="img-fluid" alt />
-                  <div className="member-content">
-                    <h4>William Anderson</h4>
-                    <span>Content</span>
-                    <p>
-                      Voluptas necessitatibus occaecati quia. Earum totam consequuntur qui porro et laborum toro des clara
-                    </p>
-                    <div className="social">
-                      <a href><i className="bi bi-twitter" /></a>
-                      <a href><i className="bi bi-facebook" /></a>
-                      <a href><i className="bi bi-instagram" /></a>
-                      <a href><i className="bi bi-linkedin" /></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+
+              ))}
+
             </div>
           </div>
         </section>{/* End Trainers Section */}
