8d1ae921183d8633c04838135b52f9c1e021e8e5
Nhat Pham <nhat2up@gmail.com>
2024-03-02 10:22:26 +0700

    learner post refund request

--- fe_learner_tutor/src/components/learner/transaction/MyTransaction.jsx
+++ fe_learner_tutor/src/components/learner/transaction/MyTransaction.jsx
@@ -3,11 +3,17 @@ import Header from '../../Header';
 import Footer from '../../Footer';
 import { Link } from 'react-router-dom';
 import learnerService from '../../../services/learner.service';
+import transactionService from '../../../services/transaction.service';
+import refundRequestService from '../../../services/refund-request.service';
+import ReactQuill from 'react-quill';
 
 const MyTransaction = () => {
     const learnerId = localStorage.getItem('learnerId');
     const [transactionList, setTransactionList] = useState([]);
     const contentRef = useRef(null);
+    const [showRefundModal, setShowRefundModal] = useState(false); // State variable for modal visibility
+    const [selectedCourseName, setSelectedCourseName] = useState(''); // State variable to store the name of the selected course
+
 
     useEffect(() => {
         learnerService
@@ -22,6 +28,42 @@ const MyTransaction = () => {
 
     const scroll = (scrollOffset) => {
         contentRef.current.scrollLeft += scrollOffset;
+    };
+
+    //REFUND
+    const [refund, setRefund] = useState({
+        reason: "",
+        transactionId: "",
+    });
+
+    const handleContentChange = (value) => {
+        setRefund({ ...refund, reason: value });
+    };
+
+    const handleRefundClick = (transactionId) => {
+        setShowRefundModal(true);
+        refund.transactionId = transactionId;
+        transactionService.getTransactionById(transactionId)
+        .then((res) => {
+            setSelectedCourseName(res.data.course.name); // Update the selected course name
+
+        })
+
+    };
+
+    const submitRefund = (e) => {
+        e.preventDefault();
+        console.log(JSON.stringify(refund))
+        // If the note is not empty, proceed with the form submission
+        refundRequestService
+            .saveRefundRequest(refund)
+            .then((res) => {
+                window.alert("You refund sent! Wait for us");
+                setShowRefundModal(false)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
     return (
@@ -50,6 +92,7 @@ const MyTransaction = () => {
                                                     <th scope="col">Amount</th>
                                                     <th scope="col">Transaction Date</th>
                                                     <th scope="col">Status</th>
+                                                    <th scope="col"></th>
                                                 </tr>
                                             </thead>
                                             <tbody>
@@ -71,11 +114,62 @@ const MyTransaction = () => {
                                                         <td>{transaction.amount} dong</td>
                                                         <td>{transaction.transactionDate}</td>
                                                         <td>{transaction.status}</td>
+                                                        <td>
+                                                            <a className='btn btn-primary' style={{ backgroundColor: '#f58d04' }} onClick={() => handleRefundClick(transaction.id)}>
+                                                                Request a refund
+                                                            </a>
+                                                        </td>
+
                                                     </tr>
+
                                                 ))}
                                             </tbody>
                                         </table>
+                                        {showRefundModal && (
+                                            <form method="post"
+                                                className="dropzone"
+                                                id="myAwesomeDropzone"
+                                                data-plugin="dropzone"
+                                                data-previews-container="#file-previews"
+                                                data-upload-preview-template="#uploadPreviewTemplate"
+                                                data-parsley-validate onSubmit={(e) => submitRefund(e)}>
+                                                <div className="modal" style={{ display: 'block', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                                                    <div className="modal-dialog modal-dialog-scrollable"> {/* Add 'modal-dialog-scrollable' class */}
+                                                        <div className="modal-content">
+                                                            <div className="modal-header">
+                                                            <h5 className="modal-title">Refund course - <span style={{ color: '#f58d04' }}>{selectedCourseName}</span> </h5>
+                                                                <button type="button" className="close" onClick={() => setShowRefundModal(false)}>
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <div className="modal-body" style={{ maxHeight: 'calc(100vh - 200px)', overflowY: 'auto' }}> {/* Set maxHeight and overflowY */}
+                                                                <ReactQuill
+                                                                    value={refund.reason}
+                                                                    onChange={handleContentChange}
+                                                                    modules={{
+                                                                        toolbar: [
+                                                                            [{ header: [1, 2, false] }],
+                                                                            [{ 'direction': 'rtl' }],
+                                                                            [{ 'align': [] }],
+                                                                            ['code-block'],
+                                                                            [{ 'color': [] }, { 'background': [] }],
+                                                                            ['clean']
+                                                                        ]
+                                                                    }}
+                                                                    theme="snow"
+                                                                    preserveWhitespace={true}
+                                                                />
 
+                                                            </div>
+                                                            <div className="modal-footer">
+                                                                <button type="button" className="btn btn-secondary" onClick={() => setShowRefundModal(false)}>Close</button>
+                                                                <button type="button" className="btn btn-primary" style={{ backgroundColor: '#f58d04' }} onClick={(e) => submitRefund(e)}>Send</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        )}
                                     </div>
                                 </div>
                             </div>
