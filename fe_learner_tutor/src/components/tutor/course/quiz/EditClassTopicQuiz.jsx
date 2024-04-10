f79dd6c045233ca25262b9ed81e78c4c9991b599
Nhat Pham <nhat2up@gmail.com>
2024-04-10 09:57:52 +0700

    edit topic quiz

--- fe_learner_tutor/src/components/tutor/course/quiz/EditClassTopicQuiz.jsx
+++ fe_learner_tutor/src/components/tutor/course/quiz/EditClassTopicQuiz.jsx
@@ -57,12 +57,67 @@ const EditClassTopicQuiz = () => {
 
 
 
+  //EDIT QUIZ
+  const [showEditQuizModal, setShowEditQuizModal] = useState(false);
 
-  const handleEditQuestion = (questionId) => {
-    // Add logic to navigate to the module edit page with the moduleId
-    navigate(`/tutor/courses/edit-topic-question/${questionId}`);
+  const openEditQuizModal = () => {
+    setShowEditQuizModal(true);
   };
 
+  const closeEditQuizModal = () => {
+    setShowEditQuizModal(false);
+  };
+
+  const handleQuizChange = (e) => {
+    const value = e.target.value;
+    setQuiz({ ...quiz, [e.target.name]: value });
+  }
+
+  const handleMinutesChange = (e) => {
+    const minutes = parseInt(e.target.value, 10);
+    setQuiz({ ...quiz, deadline: minutes });
+  };
+
+  const validateQuizForm = () => {
+    let isValid = true;
+    const errors = {};
+
+    if (quiz.name.trim() === '') {
+      errors.name = 'Quiz Name is required';
+      isValid = false;
+    }
+    if (!quiz.deadline) {
+      errors.deadline = 'Time is required';
+      isValid = false;
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
+
+  const submitQuiz = async (e) => {
+    e.preventDefault();
+
+    if (validateQuizForm()) {
+      try {
+        // Save quiz
+        console.log(JSON.stringify(quiz))
+        const quizResponse = await quizService.updateQuiz(quiz.id, quiz);
+        console.log(quizResponse.data);
+
+        const quizJson = JSON.stringify(quizResponse.data);
+
+        const quizJsonParse = JSON.parse(quizJson);
+
+        window.alert("Update Quiz Successfully!");
+        window.location.reload();
+
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <>
@@ -75,7 +130,7 @@ const EditClassTopicQuiz = () => {
             <div className="row">
               <div className="col-12">
                 <div className="card-box">
-                  <h4 className="header-title">QUIZ INFORMATION &nbsp;<i class="fa-solid fa-pen-to-square"></i></h4>
+                  <h4 className="header-title">QUIZ INFORMATION &nbsp;<i class="fa-solid fa-pen-to-square" onClick={openEditQuizModal}></i></h4>
                   <div className="table-responsive">
                     <table className="table table-borderless table-hover table-nowrap table-centered mb-0">
                       <tbody>
@@ -89,7 +144,7 @@ const EditClassTopicQuiz = () => {
                         </tr>
                         <tr>
                           <th>Times:</th>
-                          <td>{quiz.deadline}</td>
+                          <td>{quiz.deadline} minutes</td>
                         </tr>
                         <tr>
                           <th>Created Date:</th>
@@ -165,7 +220,7 @@ const EditClassTopicQuiz = () => {
 
                       <Link
                         type="button"
-                        className="btn btn-success mr-2" style={{borderRadius: '50px', padding: `8px 25px` }}
+                        className="btn btn-success mr-2" style={{ borderRadius: '50px', padding: `8px 25px` }}
                         to={`/tutor/courses/create/create-class-course/create-topic-question/${quiz.id}`}
                       >
                         Create new question
@@ -198,6 +253,115 @@ const EditClassTopicQuiz = () => {
             {/* end row*/}
 
           </div> {/* container */}
+          {showEditQuizModal && (
+            <div className="modal" tabIndex="-1" role="dialog" style={{ display: 'block', backgroundColor: 'rgba(29, 29, 29, 0.75)' }}>
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Edit Quiz</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={closeEditQuizModal}>
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  {/* Conditional rendering based on edit mode */}
+                  <>
+                    <form
+                      method="post"
+                      className="mt-3"
+                      data-plugin="dropzone"
+                      data-previews-container="#file-previews"
+                      data-upload-preview-template="#uploadPreviewTemplate"
+                      data-parsley-validate
+                      onSubmit={submitQuiz} >
+                      <div className="modal-body" style={{ maxHeight: 'calc(100vh - 200px)', overflowY: 'auto' }}>
+                        <div className="table-responsive">
+                          <table className="table table-hover">
+                            <tbody>
+                              <tr>
+                                <th style={{ width: '30%' }}>Quiz Name * :</th>
+                                <td>
+                                  <input type="text" className="form-control"
+                                    name="name" id="name" required value={quiz.name}
+                                    onChange={(e) => handleQuizChange(e)} style={{ borderRadius: '50px', padding: `8px 25px` }}
+                                  />
+                                </td>
+                              </tr>
+                              <tr>
+                                <th style={{ width: '30%' }}>Grade to pass * :</th>
+                                <td>
+                                  <select
+                                    value={quiz.gradeToPass}
+                                    onChange={(e) => setQuiz({ ...quiz, gradeToPass: e.target.value })} className="form-control"
+                                    required
+                                    style={{ borderRadius: '50px', padding: `8px 25px` }}
+
+                                  >
+                                    <option value={1}>
+                                      1
+                                    </option>
+                                    <option value={2}>
+                                      2
+                                    </option>
+                                    <option value={3}>
+                                      3
+                                    </option>
+                                    <option value={4}>
+                                      4
+                                    </option>
+                                    <option value={5}>
+                                      5
+                                    </option>
+                                    <option value={6}>
+                                      6
+                                    </option>
+                                    <option value={7}>
+                                      7
+                                    </option>
+                                    <option value={8}>
+                                      8
+                                    </option>
+                                    <option value={9}>
+                                      9
+                                    </option>
+                                    <option value={10}>
+                                      10
+                                    </option>
+                                  </select>
+                                </td>
+                              </tr>
+                              <tr>
+                                <th style={{ width: '30%' }}>Time * :</th>
+                                <td>
+                                  <select
+                                    value={quiz.deadline}
+                                    onChange={handleMinutesChange}
+                                    className="form-control"
+                                    style={{ width: '100%', borderRadius: '50px', padding: `8px 25px` }}
+                                  >
+                                    {[1, 5, 10, 15, 20, 30, 45, 60, 75, 90, 120].map((minutes) => (
+                                      <option key={minutes} value={minutes}>
+                                        {minutes} minutes
+                                      </option>
+                                    ))}
+                                  </select>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="submit" className="btn btn-success" style={{ borderRadius: '50px', padding: `8px 25px` }}>Save Changes</button>
+                        <button type="button" className="btn btn-dark" onClick={closeEditQuizModal} style={{ borderRadius: '50px', padding: `8px 25px` }}>Close</button>
+                      </div>
+                    </form>
+                  </>
+
+
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <style>
