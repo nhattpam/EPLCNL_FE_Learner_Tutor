027ca4d330ae76a5e7f04a2bb2d5e45aaa1c900c
Nhat Pham <nhat2up@gmail.com>
2024-05-31 20:25:17 +0700

    more conditions

--- fe_learner_tutor/src/components/About.js
+++ fe_learner_tutor/src/components/About.js
@@ -23,7 +23,8 @@ const About = () => {
 
                             <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
-                            <p>You have 7 days from the date of purchase to submit a refund request. Please note, for orders close to the last day of the month, the number of days in which a refund request can be submitted will be calculated from the time of purchase to the last day of the month.</p>
+                            <p>For "Video" courses, you can request a refund within 7 days of your successful purchase.
+                                 For "Class" courses, you can request a refund within 7 days of the course starting date.</p>
 
                             <h3><strong>Cookies</strong></h3>
 
